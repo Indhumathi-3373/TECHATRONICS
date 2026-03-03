0ed897a6025ff582e2a845fb1110052308665101
paulsolomon1013 <paulsolomon1013@gmail.com>
2026-03-03 14:47:03 +0530

    ""

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,16 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Login from './components/login'
+import Navbar from './components/navbar'
+import Register from './components/register'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-<<<<<<< HEAD
      <Register />;
 
-=======
-  
->>>>>>> f682273f6060b709badf4cbcbe4fe552a919b2fa
     </>
   )
 }
