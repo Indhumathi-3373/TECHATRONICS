df7dacbf205397a65b05b2717c678b22f12f3316
paulsolomon1013 <paulsolomon1013@gmail.com>
2026-03-03 14:44:43 +0530

    added register page by paul

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,10 +1,52 @@
-export default function Navbar(){
-    return (
-        <>
-        <div id="navbar">
-        <h1>hello</h1>
-        
-        </div>
-        </>
-    );
-}
+import React, { useState } from "react";
+import "./Register.css";
+
+const Register = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    alert("Registered Successfully!");
+  };
+
+  return (
+    <div className="container">
+      <div className="card">
+        <h2>Create Account</h2>
+
+        <form onSubmit={handleSubmit}>
+          <input
+            type="email"
+            placeholder="Enter Email"
+            required
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+
+          <input
+            type="password"
+            placeholder="Enter Password"
+            required
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+
+          <div className="forgot">
+            <a href="#">Forgot Password?</a>
+          </div>
+
+          <button type="submit" className="register-btn">
+            Register
+          </button>
+        </form>
+
+        <p className="signin">
+          Already have an account? <a href="#">Sign In</a>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
