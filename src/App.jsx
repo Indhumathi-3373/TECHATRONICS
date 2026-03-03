d412f934c6f00793847788ce024ab0da25384416
paulsolomon1013 <paulsolomon1013@gmail.com>
2026-03-03 14:22:45 +0530

    "i added the register page "

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,14 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Navbar from './components/navbar'
+import Register from './components/register'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    <Navbar/>
+    <Register/>
     </>
   )
 }
