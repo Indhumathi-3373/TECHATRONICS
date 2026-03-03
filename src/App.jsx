d0ad3c1c3d8302324525bdd8c9f691449f1d3986
bagavathisingh <bugzx25@gmail.com>
2026-03-03 15:18:30 +0530

    "a"

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,8 @@
 import { useState } from 'react'
 import './App.css'
-<<<<<<< HEAD
 import Card from './components/card'
-=======
 import Navbar from './components/navbar'
 import Register from './components/register'
->>>>>>> 0ed897a6025ff582e2a845fb1110052308665101
 
 function App() {
   const [count, setCount] = useState(0)
